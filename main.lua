20601ed089271dd5d30a8bef431aa56c7234f3c0
Kyariko <Syllex1314@gmail.com>
2026-06-20 17:01:24 +0200

    FIles management and creating modules folder

--- main.lua
+++ main.lua
@@ -18,10 +18,10 @@ local GUI = import("Mysterious Importer X/GUI.lua")
 local Import_Module = import("Mysterious Importer X/Import.lua")
 local Customization_Module = import("Mysterious Importer X/Customization.lua")
 
+local App = {
+    Import = Import_Module,
+    Customization = Customization_Module
+}
+
+GUI.Bind(App)
 GUI.Init_GUI()
-GUI.Bind(
-    {
-        Import = Import_Module,
-        Customization = Customization_Module
-    }
-)
