695967817a7c059302255bdbbbae2d2803a3bdc6
Kyariko <Syllex1314@gmail.com>
2026-06-23 02:58:59 +0200

    rbxm loader

--- main.lua
+++ main.lua
@@ -1,27 +1,77 @@
+local base = "https://raw.githubusercontent.com/Kyariko/Mysterious-Importer-X/dev/Modules/"
+local baserbxm = "https://raw.githubusercontent.com/Kyariko/Mysterious-Importer-X/dev/Modules/rbxm/"
+
 local cache = {}
 
-local function import(file)
-    if cache[file] then
-        return cache[file]
+local localFolder = "MIX_CACHE"
+
+if not isfolder(localFolder) then
+    makefolder(localFolder)
+end
+
+local function loadRBXM(name)
+    local path = "MIX_CACHE/" .. name .. ".rbxm"
+
+    if not isfile(path) then
+        local ok, data = pcall(function()
+            return game:HttpGet(base .. name .. ".rbxm")
+        end)
+
+        if not ok then
+            warn("Download failed:", name)
+            return nil
+        end
+
+        writefile(path, data)
     end
 
-    local code = readfile(file)
-    local fn = loadstring(code)
+    task.wait() -- 👈 important pour éviter race condition
 
-    local result = fn()
-    cache[file] = result
+    local ok, result = pcall(function()
+        return game:GetObjects(getcustomasset(path))[1]
+    end)
+
+    if not ok then
+        warn("RBXM load failed:", name, result)
+        return nil
+    end
 
     return result
 end
 
-local GUI = import("Mysterious Importer X/GUI.lua")
-local Import_Module = import("Mysterious Importer X/Import.lua")
-local Customization_Module = import("Mysterious Importer X/Customization.lua")
+local function loadModule(name)
+    if cache[name] then
+        return cache[name]
+    end
+
+    local success, code = pcall(function()
+        return game:HttpGet(base .. name .. ".lua")
+    end)
+
+    if not success then
+        warn("Failed to fetch module:", name)
+        return nil
+    end
+
+    local fn, err = loadstring(code)
+    if not fn then
+        error("Compile error in " .. name .. ": " .. err)
+    end
+
+    local module = fn()
+    cache[name] = module
+
+    return module
+end
+
+local GUI = loadModule("GUI")
+local Import = loadModule("Import")
+local Custom = loadModule("Customization")
 
 local App = {
-    Import = Import_Module,
-    Customization = Customization_Module
+    Import = Import,
+    Customization = Custom
 }
 
 GUI.Bind(App)
-GUI.Init_GUI()
+GUI.Init_GUI(loadRBXM("UI"))
